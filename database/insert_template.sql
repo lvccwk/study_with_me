6211b720d075328b59776a1d872c9bef8fe8ddf4
Lawrence <lawrence_@live.hk>
2023-01-18 15:56:55 +0800

    complete sql template update

--- database/insert_template.sql
+++ database/insert_template.sql
@@ -1,40 +1,98 @@
+-- step 1 
+INSERT INTO public.users
+(username, email, "password", "type", created_at, updated_at)
+VALUES('Tom Chan', 'tom@gmail.com', 'admin', 'student', '2023-01-18 12:18:49.673', '2023-01-18 12:18:49.673');
+INSERT INTO public.users
+(username, email, "password", "type", created_at, updated_at)
+VALUES('Mary Chan', 'mary@gmail.com', 'admin', 'student', '2023-01-18 12:18:49.674', '2023-01-18 12:18:49.674');
+INSERT INTO public.users
+(username, email, "password", "type", created_at, updated_at)
+VALUES('Peter Wong', 'peter@gmail.com', 'admin', 'teacher', '2023-01-18 12:18:49.675', '2023-01-18 12:18:49.675');
+INSERT INTO public.users
+(username, email, "password", "type", created_at, updated_at)
+VALUES('Billy Wong', 'billy@gmail.com', 'admin', 'teacher', '2023-01-18 12:18:49.676', '2023-01-18 12:18:49.676');
 
--- //blog
-INSERT INTO public.blog
-(id, teacher_id, image, "content", created_at, updated_at)
-VALUES(3, 1, 'image01.jpg', '測試中文內容', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.blog
-(id, teacher_id, image, "content", created_at, updated_at)
-VALUES(4, 2, 'image02.jpg', 'TESTCONTENT', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
--- chatroom
-INSERT INTO public.chatroom
-(id, "content", from_user, to_user, created_at, updated_at)
-VALUES(1, '有功課想問', 2, 4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.chatroom
-(id, "content", from_user, to_user, created_at, updated_at)
-VALUES(2, '教學tips', 3, 2, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+-- step 2 
+INSERT INTO public.teacher
+(user_id, created_at, updated_at)
+VALUES(3, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.teacher
+(user_id, created_at, updated_at)
+VALUES(4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
--- forum
+
+-- step 3
+INSERT INTO public.subject
+("name", created_at, updated_at)
+VALUES('chinese', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.subject
+("name", created_at, updated_at)
+VALUES('english', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.subject
+("name", created_at, updated_at)
+VALUES('maths', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.subject
+("name", created_at, updated_at)
+VALUES('econ', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+
+-- step 4 
+INSERT INTO public.teacher_subject
+(subject_id, teacher_id, created_at, updated_at)
+VALUES(2, 1, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.teacher_subject
+(subject_id, teacher_id, created_at, updated_at)
+VALUES(4, 2, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+
+-- step 5 
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('迦密主恩中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('基督教香港信義會元朗信義中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('景嶺書院', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('民生書院', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('張祝珊英文中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.school
+("name", created_at, updated_at)
+VALUES('香港培正中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+
+-- step 6
+INSERT INTO public.student
+(user_id, subject_id, school_id, academic_level, created_at, updated_at)
+VALUES(1, 2, 4, 'F.6', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.student
+(user_id, subject_id, school_id, academic_level, created_at, updated_at)
+VALUES(2, 4, 6, 'F.4', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+
+
+-- step 7
 INSERT INTO public.forum
-(id, "name", created_at, updated_at)
-VALUES(1, '學術交流板', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+("name", created_at, updated_at)
+VALUES('學術交流板', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 INSERT INTO public.forum
-(id, "name", created_at, updated_at)
-VALUES(2, '試題資源區', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+("name", created_at, updated_at)
+VALUES('試題資源區', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
-
+-- step 8
 INSERT INTO public.forum_post
-(id, "content", status, forum_id, author_id, created_at, updated_at)
-VALUES(1, '英國研究：吃塑膠包裝食品1年 等同把2個塑膠袋吃下肚', 'published', 1, 1, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+("title", status, forum_id, author_id, created_at, updated_at)
+VALUES('英國研究：吃塑膠包裝食品1年 等同把2個塑膠袋吃下肚', 'published', 1, 1, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 INSERT INTO public.forum_post
-(id, "content", status, forum_id, author_id, created_at, updated_at)
-VALUES(2, '美國宣布：對中國晶片出口管制擴大至澳門！以保護國家安全', 'pending', 2, 4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+("title", status, forum_id, author_id, created_at, updated_at)
+VALUES('美國宣布：對中國晶片出口管制擴大至澳門！以保護國家安全', 'pending', 2, 4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
-
+-- step 9
 INSERT INTO public.forum_post_comment
-(id, "content", author_id, forum_post_id, created_at, updated_at)
-VALUES(1, '英國研究團隊發現，從超市買來的塑膠包裝食材，塑膠微粒含量比傳統市場沒有包裝的食材多7倍。吃塑料包裝食品1年，相當於把2個塑膠袋吃下肚。
+("content", author_id, forum_post_id, created_at, updated_at)
+VALUES('英國研究團隊發現，從超市買來的塑膠包裝食材，塑膠微粒含量比傳統市場沒有包裝的食材多7倍。吃塑料包裝食品1年，相當於把2個塑膠袋吃下肚。
 餐桌上擺滿食材，都是準備做烤雞大餐的材料一半有塑膠包裝，一半沒有。研究人員寇賽蘿開始拆除包裝，將食物裝盤放入微波爐。這些食材都是每年耶誕節，許多家庭會享用傳統餐點。
 
 但現在英國普茲茅斯大學研究團隊發現，這些有塑膠包裝的食材，塑膠微粒含量比傳統市場未包裝的食材多很多。
@@ -51,8 +109,8 @@ VALUES(1, '英國研究團隊發現，從超市買來的塑膠包裝食材，塑
 
 專家預估，全球的塑膠生產量將會在20年內翻倍成長，屆時流入海洋的廢塑膠數量，預計也將會在這時期內增加為2倍。', 1, 1, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 INSERT INTO public.forum_post_comment
-(id, "content", author_id, forum_post_id, created_at, updated_at)
-VALUES(2, 'The holiday dinner many are looking forward to this season could come with an unexpected ingredient— plastic.
+("content", author_id, forum_post_id, created_at, updated_at)
+VALUES('The holiday dinner many are looking forward to this season could come with an unexpected ingredient— plastic.
 That’s because a new study published in November from researchers at the University of Portsmouth in England has found that a roast dinner can contain a “staggering” number of microplastics.
 Microplastic particles are plastics that are smaller than five millimetres in size, according to a press release from the school.
 The university’s study explored whether meal ingredients packaged in plastic would result in more microplastic particles found in the food items
@@ -70,73 +128,19 @@ The researchers found that the intake of microplastics by humans has been well-e
 However, research published in 2021 by U.K. researchers found that microplastics can cause damage to human cells in the laboratory at levels that are considered normal for people to ingest through their food. But the health impact remains unclear, as researchers are unsure how long plastics remain in the body before they exit through waste.', 4, 2, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
 
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(1, '迦密主恩中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(2, '基督教香港信義會元朗信義中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(3, '景嶺書院', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(4, '民生書院', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(5, '張祝珊英文中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.school
-(id, "name", created_at, updated_at)
-VALUES(6, '香港培正中學', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+-- step 10
+INSERT INTO public.chatroom
+("content", from_user, to_user, created_at, updated_at)
+VALUES('有功課想問', 2, 4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.chatroom
+("content", from_user, to_user, created_at, updated_at)
+VALUES('教學tips', 3, 2, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
 
 
-
-INSERT INTO public.student
-(id, user_id, subject_id, school_id, academic_level, created_at, updated_at)
-VALUES(1, 1, 2, 4, 'F.6', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.student
-(id, user_id, subject_id, school_id, academic_level, created_at, updated_at)
-VALUES(2, 2, 4, 6, 'F.4', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-
-INSERT INTO public.subject
-(id, "name", created_at, updated_at)
-VALUES(1, 'chinese', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.subject
-(id, "name", created_at, updated_at)
-VALUES(2, 'english', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.subject
-(id, "name", created_at, updated_at)
-VALUES(3, 'maths', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.subject
-(id, "name", created_at, updated_at)
-VALUES(4, 'econ', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-
-
-INSERT INTO public.teacher
-(id, user_id, created_at, updated_at)
-VALUES(1, 3, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.teacher
-(id, user_id, created_at, updated_at)
-VALUES(2, 4, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-
-
-INSERT INTO public.teacher_subject
-(id, subject_id, teacher_id, created_at, updated_at)
-VALUES(1, 2, 1, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-INSERT INTO public.teacher_subject
-(id, subject_id, teacher_id, created_at, updated_at)
-VALUES(2, 4, 2, '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
-
-
-INSERT INTO public.users
-(id, username, email, "password", "type", created_at, updated_at)
-VALUES(1, 'Tom Chan', 'tom@gmail.com', 'admin', 'student', '2023-01-18 12:18:49.673', '2023-01-18 12:18:49.673');
-INSERT INTO public.users
-(id, username, email, "password", "type", created_at, updated_at)
-VALUES(2, 'Mary Chan', 'mary@gmail.com', 'admin', 'student', '2023-01-18 12:18:49.674', '2023-01-18 12:18:49.674');
-INSERT INTO public.users
-(id, username, email, "password", "type", created_at, updated_at)
-VALUES(3, 'Peter Wong', 'peter@gmail.com', 'admin', 'teacher', '2023-01-18 12:18:49.675', '2023-01-18 12:18:49.675');
-INSERT INTO public.users
-(id, username, email, "password", "type", created_at, updated_at)
-VALUES(4, 'Billy Wong', 'billy@gmail.com', 'admin', 'teacher', '2023-01-18 12:18:49.676', '2023-01-18 12:18:49.676');
+-- step 11
+INSERT INTO public.blog
+(teacher_id, image, "content", created_at, updated_at)
+VALUES(1, 'image01.jpg', '測試中文內容', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
+INSERT INTO public.blog
+(teacher_id, image, "content", created_at, updated_at)
+VALUES(2, 'image02.jpg', 'TESTCONTENT', '2023-01-18 12:18:49.700', '2023-01-18 12:18:49.700');
