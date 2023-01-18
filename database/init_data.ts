2509355d88ac7cc2f3c47cd00e68f4bf21556360
James Pui <jclpui@gmail.com>
2023-01-18 13:24:26 +0800

    update

--- database/init_data.ts
+++ database/init_data.ts
@@ -1,18 +1,17 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import pg from 'pg'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 const client = new pg.Client({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-});
+	database: process.env.DB_NAME,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD
+})
 
 async function main() {
-  await client.connect(); // "dial-in" to the postgres server
+	await client.connect() // "dial-in" to the postgres server
 
 	// const data = [{id:1,name: "Tom Chan",},{id:2,name:"Mary Chan"}]
-
 
 	// for (let datum of data){
 	// 	await client.query(`INSERT INTO users (id,username,email,password,type,created_at,updated_at) values (${datum.id},${datum.name},$3,$4,$5,now(),now())`);
@@ -22,18 +21,19 @@ async function main() {
 	// "1","Tom Chan","tom@gmail.com","admin","student"
 	// // "2","Mary Chan","mary@gmail.com","marychan","teacher"
 	// ]);
-	await client.query("INSERT INTO users (id,username,email,password,type,created_at,updated_at) values ($1,$2,$3,$4,$5,now(),now())", [
-	"1","Tom Chan","tom@gmail.com","admin","student"
-	]);
+	await client.query(
+		'INSERT INTO users (id,username,email,password,type,created_at,updated_at) values ($1,$2,$3,$4,$5,now(),now())',
+		['1', 'Tom Chan', 'tom@gmail.com', 'admin', 'student']
+	)
 	// await client.query("INSERT INTO users (id,username,email,password,type,created_at,updated_at) values ($1,$2,$3,$4,$5,now(),now())", [
 	// "3","Tom Chan","tom@gmail.com","tomchan","student"
 	// ]);
-	
+
 	// //teacher - 02
 	// await client.query("INSERT INTO teacher (id,user_id,created_at,updated_at) values ($1,$2,now(),now())", [
 	// "2001","1001"
 	// ]);
-	
+
 	// //subject - 03
 	// await client.query("INSERT INTO subject (id,name,created_at,updated_at) values ($1,$2,now(),now())", [
 	// "3001","chinese"
@@ -53,79 +53,37 @@ async function main() {
 	// await client.query("INSERT INTO school (id,name,created_at,updated_at) values ($1,$2,now(),now())", [
 	// "7001","1001school"
 	// ]);
-	
+
 	// // student - 06
 	// await client.query("INSERT INTO student (id,subject_id,academic_level,school_id,user_id,subject, created_at,updated_at) values ($1,$2,$3,$4,$5,$6,now(),now())", [
 	// "6001","3001", "F6","7001","1001","chinese"
 	// ]);
 
-		
 	// // chatroom - 08
 	// await client.query("INSERT INTO chatroom (id,content,from_user,to_user,created_at,updated_at) values ($1,$2,$3,$4,now(),now())", [
 	// 	"8001","content1001","1001","1001"
 	// ]);
-	
+
 	// // forum - 11
 	// await client.query("INSERT INTO forum (id,name,created_at,updated_at) values ($1,$2,now(),now())", [
 	// 	"11001", "forum"
 	// ]);
-	
+
 	// // forum_post - 09
 	// await client.query("INSERT INTO forum_post (id,forum_id,content,status,author_id,created_at,updated_at) values ($1,$2,$3,$4,$5,now(),now())", [
 	// 	"9001", "11001","content1001","published","1001"
 	// ]);
-	
+
 	// // // forum_post_comment - 10
 	// await client.query("INSERT INTO forum_post_comment (id,content,author_id,forum_post_id,created_at,updated_at) values ($1,$2,$3,$4,now(),now())", [
 	// 	"10001","content001","1001", "9001"
 	// ]);
 
-
-	await client.end(); // close connection with the database
+	await client.end() // close connection with the database
 }
-main();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main()
 
 // ----------------------------------------17/1/2023, first time connect database records -------------------------------------------
-
 
 //   const user = {
 // 	id:"1001",
@@ -144,7 +102,7 @@ main();
 // 	await client.query("INSERT INTO teacher (id,user_id,created_at,updated_at) values ($1,$2,now(),now())", [
 // 	"2001","1001"
 // 	]);
-	
+
 // 	//subject - 03
 // 	await client.query("INSERT INTO subject (id,name,created_at,updated_at) values ($1,$2,now(),now())", [
 // 	"3001","chinese"
@@ -164,13 +122,12 @@ main();
 // 	await client.query("INSERT INTO school (id,name,created_at,updated_at) values ($1,$2,now(),now())", [
 // 	"7001","1001school"
 // 	]);
-	
+
 // 	// student - 06
 // 	await client.query("INSERT INTO student (id,subject_id,academic_level,school_id,user_id,subject, created_at,updated_at) values ($1,$2,$3,$4,$5,$6,now(),now())", [
 // 	"6001","3001", "F6","7001","1001","chinese"
 // 	]);
 
-	
 // // chatroom - 08
 // await client.query("INSERT INTO chatroom (id,content,from_user,to_user,created_at,updated_at) values ($1,$2,$3,$4,now(),now())", [
 // 	"8001","content1001","1001","1001"
@@ -196,5 +153,3 @@ main();
 //   ]);
 
 //   console.log(result.rows[0].username); // gordon
-
-
